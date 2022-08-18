81d812538c435a2b3af45df069ae19c5bbc69b65
goverclock <goverclock@outlook.com>
2022-08-18 08:56:04 +0800

    README

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # boosteed-reading-en-extension
 
+A simple implementation of [Bionic Reading](https://bionic-reading.com/).
 
+Todo:
+
+- [ ] Implement different modes:
+    1. Always on
+    2. Always off
+    3. Manual
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const paras = document.querySelectorAll("p");
+const paras = document.getElementsByTagName("p");
 
 for (let i = 0; i < paras.length; i++) {
   let arr = paras[i].textContent.split(" ");
 
   let tmparr = [];
   for (let word of arr) {
-    if (word.length >= 3) {
-      word = "<strong>" + word.slice(0, 3) + "</strong>" + word.slice(3);
-    } else {
+    if (word.length <= 3) {
       word = "<strong>" + word + "</strong>";
+    } else {
+      word = "<strong>" + word.slice(0, 3) + "</strong>" + word.slice(3);
     }
 
     tmparr.push(word);
@@ -16,5 +16,3 @@ for (let i = 0; i < paras.length; i++) {
   paras[i].innerHTML = tmparr.join(" ");
 
 }
-
-// alert("FUCK")
